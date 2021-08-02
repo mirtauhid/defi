285e8e1f475f0d1bffe32f806a74a26f660ea8e1
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2021-08-02 09:44:55 -0700

    for fix

---
+++ README.md
@@ -0,0 +1,8 @@
+ Frontend
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/66319811-32db-485e-ae07-ae9431b16f46/deploy-status)](https://app.netlify.com/sites/naughty-goodall-df47f6/deploys)
+
+This project contains the main features of the SaltSwap application.
+
+If you want to contribute, please refer to the [contributing guidelines](./CONTRIBUTING.md) of this project.
+# saltswap-frontend-main
---
+++ src/components/Menu/config.ts
@@ -0,0 +1,110 @@
+import { MenuEntry } from '@saltswap/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+      {
+        label: 'Exchange',
+        href: 'https://exchange.owld.app/#/swap',
+        icon: "ExchangeIcon",
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.owld.app/#/pool',
+        icon: "LiquidityIcon",
+      },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: '/pools',
+  },
+  {
+  label:'Lottery',
+  icon: 'TicketIcon',
+  href: '/lottery',
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  // {
+  //   label: 'Teams & Profile',
+  //   icon: 'GroupsIcon',
+  //   calloutClass: 'rainbow',
+  //   items: [
+  //     {
+  //       label: 'Leaderboard',
+  //       href: '/teams',
+  //     },
+  //     {
+  //       label: 'Task Center',
+  //       href: '/profile/tasks',
+  //     },
+  //     {
+  //       label: 'Your Profile',
+  //       href: '/profile',
+  //     },
+  //   ],
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://saltswap.info',
+      },
+      {
+        label: 'Tokens',
+        href: 'https://saltswap.info/tokens',
+      },
+      {
+        label: 'Pairs',
+        href: 'https://saltswap.info/pairs',
+      },
+      {
+        label: 'Accounts',
+        href: 'https://saltswap.info/accounts',
+      },
+    ],
+  },
+  // {
+  //   label: 'IFO',
+  //   icon: 'IfoIcon',
+  //   href: '/ifo',
+  // },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/owldefi',
+      },
+      {
+        label: 'Docs',
+        href: 'https://owld.gitbook.io/docs/',
+      },
+      {
+        label: 'Blog',
+        href: 'https://medium.com/@owldefi',
+      },
+    ],
+  },
+]
+
+export default config
